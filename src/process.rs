3761c1bda49597ac5eedd8920025c9dd1fe66f97
tigershu86 <ming.shu@outlook.com>
2024-05-04 23:51:39 +0800

    refect code

---
+++ src/process.rs
@@ -0,0 +1,32 @@
+use csv::Reader;
+use serde::{Serialize, Deserialize};
+use std::fs;
+use anyhow;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Player {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::with_capacity(128);
+    for result in reader.deserialize() {
+        let record:Player = result?;
+        ret.push(record);
+    }
+
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+
+    Ok(())
+}
